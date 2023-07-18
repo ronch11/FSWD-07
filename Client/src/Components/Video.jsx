cd048bebbd427fe6d6cfb1ce4b6c1a2b7fd6a593
CupKido <taler@g.jct.ac.il>
2023-07-18 20:35:59 +0300

    added comments to video

--- Client/src/Components/Video.jsx
+++ Client/src/Components/Video.jsx
@@ -3,7 +3,8 @@ import { useUser } from "../UserContext";
 import { useEffect, useState, useContext } from "react";
 import ApiContext from "../ApiContext";
 import { useParams, useNavigate } from "react-router-dom";
-
+import Comment from './Comment';
+import CommentSubmitter from './CommentSubmitter';
 function Video() {
     let {videoid} = useParams();
     const api = useContext(ApiContext);
@@ -12,25 +13,30 @@ function Video() {
     const [reaction, setReaction] = useState('');
     const [dislikes, setDislikes] = useState(0);
     const [likes, setLikes] = useState(0);
+    const [comments, setComments] = useState([]);
     const [videoUrl, setVideoUrl] = useState('');
     console.log(videoid);  
     useEffect(() => {
         // set access token
         api.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('access_token');
+        // request video details
         api.get(`/videos/details/${videoid}`).then(response => {
             console.log(response);
             setVideoDetails(response.data);
             setLikes(response.data.reactions.like);
             setDislikes(response.data.reactions.dislike);
+            if(response.data.reactions.user) setReaction(response.data.reactions.user);
         }).catch(error => {
             console.log(error);
-            if (error.response.status === 401){
+            if (error.status === 401){
                 navigate('/Login');
             }
-            else if (error.response.status === 404){
+            else if (error.status === 404){
                 navigate('/404');
             }
         });
+
+        // request video
         // set response type to blob for this request
         api.get(api.defaults.baseURL + '/videos/watch/' + videoid, {
             responseType: 'blob'
@@ -42,9 +48,20 @@ function Video() {
             setVideoUrl(url);
 
           });
+
+        // request comments
+        api.get('/comments/' + videoid).then(response => {
+            console.log(response);
+            setComments(response.data);
+        })
     }, [])
 
     function likeVideo(){
+        // check if access token is present
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken){
+            navigate('/Login');
+        }
         if (reaction !== 'like'){
             api.post(`/videos/react/${videoid}`, {
                 reaction: 'like'
@@ -62,6 +79,10 @@ function Video() {
     }
 
     function dislikeVideo(){
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken){
+            navigate('/Login');
+        }
         api.post(`/videos/react/${videoid}`, {
             reaction: 'dislike'
         }).then(response => {
@@ -71,11 +92,14 @@ function Video() {
             }
             setDislikes(dislikes + 1);
             setReaction('dislike');
-        }
-        );
+        });
     }
 
     function clearReaction(){
+        const accessToken = localStorage.getItem('access_token');
+        if (!accessToken){
+            navigate('/Login');
+        }
         api.post(`/videos/react/${videoid}`, {
             reaction: ''
         }).then(response => {
@@ -91,6 +115,13 @@ function Video() {
         );
     }
 
+    const copyLink = async (e) => {
+        e.preventDefault();
+        const currentUrl = window.location.href;
+        await navigator.clipboard.writeText(currentUrl);
+        alert('Link copied to clipboard');
+    }
+
     function detailsElem(){
         if (videoDetails){
             return (
@@ -100,6 +131,16 @@ function Video() {
                     <p>Views: {videoDetails.views}</p>
                     <button className={reaction === 'like' ? 'ButtonPressed' : ''} onClick={reaction === 'like' ? clearReaction : likeVideo}>Likes: {likes}</button>
                     <button className={reaction === 'dislike' ? 'ButtonPressed' : ''} onClick={reaction === 'dislike' ? clearReaction : dislikeVideo}>Dislikes: {dislikes}</button>
+                    <button onClick={copyLink}>Share</button>
+                    <div>
+                        <p>Comments</p>
+                        <CommentSubmitter videoid={videoid} addComment={(comment) => {setComments([...comments, comment])}} />
+                        <ul>
+                            { comments.map(comment => {
+                                return <li><Comment comment={comment}/></li>
+                            })}
+                        </ul>
+                    </div>
                 </div>
             )
         }
