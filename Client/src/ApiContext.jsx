4b8f32800ac9b9ef8b0de37990240b02fcbc13f7
CupKido <taler@g.jct.ac.il>
2023-07-24 19:25:30 +0300

    finished moving to http

--- Client/src/ApiContext.jsx
+++ Client/src/ApiContext.jsx
@@ -5,7 +5,7 @@ const ApiContext = React.createContext();
 
 export const ApiProvider = ({children}) => {
   const api = axios.create({
-    baseURL: 'https://localhost:8000/api'
+    baseURL: 'http://localhost:8000/api'
   });
 
   return (
