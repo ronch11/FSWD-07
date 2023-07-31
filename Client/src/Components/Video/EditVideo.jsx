5bd1648b0d6d64c059a983d9c862ed2a90f98939
ron hodadedi <hodadedi@g.jct.ac.il>
2023-07-31 16:46:31 +0300

    update

--- Client/src/Components/Video/EditVideo.jsx
+++ Client/src/Components/Video/EditVideo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect, useContext } from 'react';
 import ApiContext from '../../ApiContext.jsx';
+import "../../Styles/EditVideo.css";
 function EditVideo() {
     const api = useContext(ApiContext);
     const navigate = useNavigate();
@@ -10,6 +11,8 @@ function EditVideo() {
     const [description, setDescription] = useState('');
     const [tags, setTags] = useState('');
     const [visibility, setVisibility] = useState('');
+    const [videoUrl, setVideoUrl] = useState('');
+    const {vidoeimg, setVidoeimg} = useState('');
     api.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('access_token');
 
     useEffect(() => {
@@ -19,6 +22,7 @@ function EditVideo() {
             setDescription(response.data.description);
             setTags(response.data.tags.join(','));
             setVisibility(response.data.visibility);
+            setVidoeimg(response.data.thumbnail)
         }).catch(error => {
             console.log(error);
         });
@@ -38,37 +42,54 @@ function EditVideo() {
             console.log(error);
         });
     }
+    
+
+
+    
 
   return (
-    <div>
-          <form onSubmit={updateVideo}>
-            <label>
+    <>
+
+    <div className='video-edite'>
+    <img width={320} src={'/videos/thumb/' + videoid}></img>
+
+    </div>
+
+
+
+
+    
+    <div className='update-video'>
+          <form className='form1-update' onSubmit={updateVideo}>
+            <label className='lab-up'>
                 Title:
-                <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+                <input className='input-text-up' type="text" value={title} onChange={e => setTitle(e.target.value)} />
             </label>
 
-            <label>
+            <label className='lab-up'>
                 Description:
-                <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
+                <textarea className='text-erae-up' value={description} onChange={e => setDescription(e.target.value)}></textarea>
             </label>
 
-            <label>
-                Tags (comma separated):
-                <input type="text" value={tags} onChange={e => setTags(e.target.value)} />
+            <label className='lab-up'>
+                Tags:
+                <input className='tags-input' type="text" value={tags} onChange={e => setTags(e.target.value)} />
             </label>
 
             <label>
                 Visibility:
-                <select value={visibility} onChange={e => setVisibility(e.target.value)}>
+                <select className='select-edite-vidoe' value={visibility} onChange={e => setVisibility(e.target.value)}>
                   <option value="public">Public</option>
                   <option value="private">Private</option>
                   <option value="unlisted">Unlisted</option>
                 </select>
             </label>
 
-            <input type="submit" value="Submit" />
+            <input className='sub-edi' type="submit" value="Submit" />
           </form>
       </div>
+
+     </> 
   )
 }
 
