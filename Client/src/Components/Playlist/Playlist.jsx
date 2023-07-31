9d6b3b649a27f8cd3991fa1af0d1038c4abb1ab8
CupKido <taler@g.jct.ac.il>
2023-07-31 14:25:56 +0300

    fixed view playlist bug

--- Client/src/Components/Playlist/Playlist.jsx
+++ Client/src/Components/Playlist/Playlist.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ApiContext from '../../ApiContext';
 import VideoList from '../Video/VideoList';
 function Playlist() {
@@ -8,6 +9,7 @@ function Playlist() {
     const [playlistName, setPlaylistName] = useState('');
     let { playlistId } = useParams();
     const api = useContext(ApiContext);
+    const navigate = useNavigate();
     useEffect(() => {
         //Get videos details from the server
         const token = localStorage.getItem('access_token')
@@ -15,18 +17,38 @@ function Playlist() {
         api.defaults.headers.common['Authorization'] = `Bearer ${token}`
         api.get('/playlists/light/' + playlistId).then((response) => {
             const playlist = response.data;
-            console.log(playlist);
-            setVideos(playlist.videos);
+            setVideos(playlist.videos.map(video => { return { _id: video } }));
             setPlaylistName(playlist.name);
         }
         ).catch((error) => {
             console.log(error)
         });
     }, [])
-    return (
-        <div>{
-                videos.length > 0 ? <h3>{playlistName}</h3> : <h3>The Playlist is empty!</h3>
+
+    const handleDeletePlaylist = (e) => {
+        e.preventDefault();
+        let confirmAction = confirm("Are you sure you want to delete this playlist?");
+        if (confirmAction){
+            api.delete('/playlists/' + playlistId).then((response) => {
+                console.log(response);
+                navigate('/Playlists');
+            }).catch((error) => {
+                console.log(error);
             }
+            );
+        }else{
+            return;
+        }
+    }
+
+    return (
+        
+        <div>
+            <h2>{playlistName}</h2>
+            {
+                videos.length > 0 ? '' : <h3>The Playlist is empty!</h3>
+            }
+            <button onClick={handleDeletePlaylist}>Delete Playlist</button>
             <VideoList videos={videos} detailsIncluded={false} />   
         </div>
     )
