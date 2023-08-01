7cf790c63d943c35571b70575940d782b44164f4
CupKido <taler@g.jct.ac.il>
2023-08-01 14:03:55 +0300

    added deletion from playlist

--- Client/src/Components/Playlist/Playlist.jsx
+++ Client/src/Components/Playlist/Playlist.jsx
@@ -4,17 +4,20 @@ import { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ApiContext from '../../ApiContext';
 import VideoList from '../Video/VideoList';
+import { useLoadingUpdate } from '../../LoadingContext';
 function Playlist() {
     const [videos, setVideos] = useState([]);
     const [playlistName, setPlaylistName] = useState('');
     let { playlistId } = useParams();
     const api = useContext(ApiContext);
     const navigate = useNavigate();
+    const setLoading = useLoadingUpdate();
     useEffect(() => {
         //Get videos details from the server
         const token = localStorage.getItem('access_token')
         // set the authorization header
         api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+        setLoading(true);
         api.get('/playlists/light/' + playlistId).then((response) => {
             const playlist = response.data;
             setVideos(playlist.videos.map(video => { return { _id: video } }));
@@ -22,6 +25,8 @@ function Playlist() {
         }
         ).catch((error) => {
             console.log(error)
+        }).finally(() => {
+            setLoading(false);
         });
     }, [])
 
@@ -40,6 +45,23 @@ function Playlist() {
             return;
         }
     }
+    const removeFromPlaylist = (playlistId) => {
+        api.delete(`/playlists/remove/${playlistId}/${videoId}`).then((response) => {
+            console.log(response.data)
+            setSelectedPlaylists(selectedPlaylists.filter((id) => {
+                return id !== playlistId
+            }))
+        }
+        ).catch((error) => {
+            console.log(error)
+        }
+        );
+    }
+    const handleRemoveFromPlaylist = (e) => {
+        e.preventDefault();
+        const videoId = e.target.value;
+        removeFromPlaylist(playlistId, videoId);
+    }
 
     return (
         
@@ -49,7 +71,7 @@ function Playlist() {
                 videos.length > 0 ? '' : <h3>The Playlist is empty!</h3>
             }
             <button onClick={handleDeletePlaylist}>Delete Playlist</button>
-            <VideoList videos={videos} detailsIncluded={false} />   
+            <VideoList videos={videos} detailsIncluded={false} DeleteVideo={handleDeletePlaylist} />   
         </div>
     )
 }
